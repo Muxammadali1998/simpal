d860006832379219828cdf70b25ff02730cd6a9e
Muxammadali98 <turgunaliyev.98@gmail.com>
2023-02-14 11:09:40 +0500

    pusherOk

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,10 +36,9 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
-                'useTLS' => true,
-            ],
+                'cluster' => 'ap2',
+                'useTLS' => true
+              ],
         ],
 
         'ably' => [
